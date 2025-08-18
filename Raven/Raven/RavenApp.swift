e1001ee4d7cb8b736d4cee53a0a68648120750f4
Matheus Silva <124085006+silvaaszx@users.noreply.github.com>
2025-08-18 15:55:33 -0300

    Adiciona estrutura inicial do projeto Xcode

---
+++ Raven/Raven/RavenApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavenApp.swift
+//  Raven
+//
+//  Created by Matheus Silva on 18/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RavenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Raven/RavenTests/RavenTests.swift
@@ -0,0 +1,17 @@
+//
+//  RavenTests.swift
+//  RavenTests
+//
+//  Created by Matheus Silva on 18/08/25.
+//
+
+import Testing
+@testable import Raven
+
+struct RavenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
