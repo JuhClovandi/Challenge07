7cf218856d283b1fa25923f6fee7fccbd35a24e3
Kenay <kenay@10-47-48-48.0200.ucb.br.iosda.org>
2025-09-02 14:43:44 -0300

    Finish first part of Onboarding

--- Raven/Raven/RavenApp.swift
+++ Raven/Raven/RavenApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct RavenApp: App {
     var body: some Scene {
         WindowGroup {
-//            Text("oi")
-            OnboardingView()
+            ContentView()
         }
     }
 }
