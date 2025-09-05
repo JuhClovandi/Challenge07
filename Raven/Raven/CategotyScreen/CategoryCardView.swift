3df8c9cd41a1b55497e6b02c329eb5a3aa5b0c56
Kenay <kenay@10-47-48-48.0200.ucb.br.iosda.org>
2025-09-05 16:23:18 -0300

    Finish Categoy Screen

---
+++ Raven/Raven/CategotyScreen/CategoryCardView.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryCardView.swift
+//  Raven
+//
+//  Created by Kenay on 05/09/25.
+//
+
+import SwiftUI
+
+// Subview para o card da categoria
+struct CategoryCardView: View {
+    let categoryName: String
+    let textColor = Color("Title")
+    let cardColor = Color("CardBackground")
+
+    var body: some View {
+        Button(action: {
+            // Ação ao tocar na categoria
+            print("Categoria \(categoryName) selecionada.")
+        }) {
+            HStack(spacing: 20) {
+                Image(systemName: "photo.on.rectangle.angled") // Ícone padrão
+                    .frame(width: 100, height: 100)
+                    .font(.system(size: 40))
+                    .foregroundStyle(textColor)
+                    .padding()
+                    .background(Color.white.opacity(0.1))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+
+                Text(categoryName)
+                    .font(.custom("DMSerifText-Regular", size: 32, relativeTo: .title2))
+                    .fontWeight(.semibold)
+                    .foregroundStyle(textColor)
+                
+                Spacer()
+            }
+            .frame(height: 120)
+            .padding()
+            .background(cardColor)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+        }
+    }
+}
